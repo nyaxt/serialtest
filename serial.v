e252180fe5a787a9336be6d1840b165c4df4d939
nyaxt <ueno@nyaxtstep.com>
2013-05-26 16:33:24 +0900

    rx impl.

--- serial.v
+++ serial.v
@@ -2,61 +2,118 @@
 
 module serial(
     output reg tx,
+    output reg [7:0] dat_r,
+    output reg ready,
     input clk,
-    input [7:0] data,
-    input txe
+    input rst,
+    input rx,
+    input [7:0] dat_t,
+    input txe,
+    input ready_rst
     );
 parameter CLK_FREQ = 50_000_000;
 parameter BAUD = 9600;
 parameter CLK_MUL = CLK_FREQ / (BAUD * 16);
 parameter CLK_MUL_WIDTH = $ceil($log10(CLK_MUL)/$log10(2));
 
+// clk multiplier: baudtick16 / baudtick
 reg [(CLK_MUL_WIDTH-1):0] baudcounter;
-initial baudcounter = 0;
-wire baudtick16 = (baudcounter == CLK_MUL);
+wire baudtick16 = (baudcounter == (CLK_MUL-1));
 
 always @(posedge clk)
-    if(baudtick16)
+    if(rst | baudtick16)
         baudcounter <= 0;
     else
         baudcounter <= baudcounter + 1;
 
 reg [3:0] baudcounter2;
-initial baudcounter2 = 0;
-wire baudtick = baudtick16 & (baudcounter2 == 4'hf);
+wire baudtick = baudtick16 & (baudcounter2 == 4'h0);
 always @(posedge clk)
-    if(baudtick16) begin
+    if(rst)
+        baudcounter2 <= 0;
+    else if(baudtick16)
         baudcounter2 <= baudcounter2 + 1;
-    end
 
-reg [3:0] state;
-initial state = 4'h0;
+// ====== Tx ======
+reg [7:0] dat_t_ff;
 
-// advance state
+reg [3:0] tx_state;
+
+// advance tx_state
 always @(posedge clk)
-    if(txe)
-        state <= 4'h1;
-    else if(baudtick)
-        if(state == 11)
-            state <= 4'h0;
-        else if(state != 0)
-            state <= state + 1;
+    if(rst)
+        tx_state = 4'h0;
+    else if(txe) begin
+        tx_state <= 4'h1;
+        dat_t_ff <= dat_t;
+    end else if(baudtick)
+        if(tx_state == 11)
+            tx_state <= 4'h0;
+        else if(tx_state != 0)
+            tx_state <= tx_state + 1;
 
 // output tx
-always @(state[3:0] or data[7:0])
-    casex(state[3:0])
+always @(tx_state[3:0] or dat_t[7:0])
+    casex(tx_state[3:0])
         0: tx <= 1'b1; // idle 1
         1: tx <= 1'b1; // rts
         2: tx <= 1'b0; // start bit
-        3: tx <= data[0];
-        4: tx <= data[1];
-        5: tx <= data[2];
-        6: tx <= data[3];
-        7: tx <= data[4];
-        8: tx <= data[5];
-        9: tx <= data[6];
-        10: tx <= data[7];
+        3: tx <= dat_t_ff[0];
+        4: tx <= dat_t_ff[1];
+        5: tx <= dat_t_ff[2];
+        6: tx <= dat_t_ff[3];
+        7: tx <= dat_t_ff[4];
+        8: tx <= dat_t_ff[5];
+        9: tx <= dat_t_ff[6];
+        10: tx <= dat_t_ff[7];
         11: tx <= 1'b1; // end bit
     endcase
+
+// ====== Rx ======
+reg rx_active;
+reg [7:0] rx_counter;
+
+always @(posedge clk) begin
+    if(ready_rst)
+        ready <= 0;
+
+    if(rst) begin
+        rx_active <= 0;
+        rx_counter <= 8'h00;
+        dat_r <= 8'h0;
+        ready <= 0;
+    end else if(!rx_active) begin
+        // start bit
+        if(rx == 1'b0) begin
+            rx_active <= 1;
+            rx_counter <= 8'h00;
+            ready <= 0;
+        end
+    end else if(baudtick16) begin
+        rx_counter <= rx_counter + 1;
+        
+        // try read at middle
+        if(rx_counter[3:0] == 4'h8) begin
+            case(rx_counter[7:4])
+                0: begin
+                    // reset if false start
+                    if(rx == 1'b1)
+                        rx_active <= 0;
+                end
+                // end of cycle
+                9: begin
+                    rx_active <= 0;
+                end
+                default: begin
+                    dat_r <= {rx, dat_r[7:1]};
+
+                    // set ready on last bit recv
+                    if(rx_counter[7:4] == 8)
+                        ready <= 1;
+                end 
+            endcase
+        end
+    end
+end
 
 endmodule
--- serial_t.v
+++ serial_t.v
@@ -3,15 +3,25 @@
 module serial_t;
 // Inputs
 reg clk;
-reg [7:0] data;
+reg rst;
+reg rx;
+reg [7:0] dat_t;
 reg txe;
+reg ready_rst;
 
 // Instantiate the Unit Under Test (UUT)
 serial uut (
 	.clk(clk), 
-	.data(data), 
-	.txe(txe)
+    .rst(rst),
+    .rx(rx),
+	.dat_t(dat_t),
+	.txe(txe),
+    .ready_rst(ready_rst)
 );
+
+parameter BAUD_WAIT = 1_000_000_000 / 9600;
+
+reg [7:0] test_rx;
 
 initial begin
 	$dumpfile("serial_t.lxt");
@@ -24,9 +34,17 @@ initial begin
     $display("CLK_MUL_WIDTH: %d", uut.CLK_MUL_WIDTH);
 
 	// Initialize Inputs
-	data = 8'h59;
-	txe = 0;
     clk = 0;
+    rst = 0;
+    rx = 1;
+	dat_t = 8'h59;
+	txe = 0;
+    ready_rst = 0;
+
+    #10;
+    rst = 1;
+    #10;
+    rst = 0;
 
 	// Wait 10us for global reset to finish
 	#10_000;
@@ -36,11 +54,39 @@ initial begin
 	#20;
 	txe = 0;
 
+    // recv some data
+    test_rx = 8'h34;
+
+    $display("BAUD_WAIT: %d", BAUD_WAIT);
+    rx = 0;
+    #(BAUD_WAIT);
+    rx = test_rx[0];
+    #(BAUD_WAIT);
+    rx = test_rx[1];
+    #(BAUD_WAIT);
+    rx = test_rx[2];
+    #(BAUD_WAIT);
+    rx = test_rx[3];
+    #(BAUD_WAIT);
+    rx = test_rx[4];
+    #(BAUD_WAIT);
+    rx = test_rx[5];
+    #(BAUD_WAIT);
+    rx = test_rx[6];
+    #(BAUD_WAIT);
+    rx = test_rx[7];
+    #(BAUD_WAIT);
+    rx = 1;
+
+    ready_rst = 1;
+    #20;
+    ready_rst = 0;
+
 	#1_000_000;
 	$finish(2);
 end
 
-always #5 clk = ~clk;
+always #10 clk = ~clk;
 
 endmodule
 
