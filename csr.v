1dcf673ef8c2209406626e1e545a894f93c8bb64
Kouhei Ueno <ueno@nyaxtstep.com>
2013-05-29 00:32:40 +0900

    make synthesizable

--- csr.v
+++ csr.v
@@ -1,9 +1,13 @@
 // configuration status registers via wishbone bus
-module csr(
+module csr #(
+    parameter NUM_CH = 4,
+    parameter VOL_WIDTH = (8*2*NUM_CH),
+    parameter ADDR_OFFSET_VOLS = 8'h00
+)(
     input clk,
     input rst,
 
-    output [(VOL_WIDTH):0] vol,
+    output [(VOL_WIDTH-1):0] vol,
 
     input [6:0] wb_adr_i,
     input [7:0] wb_dat_i,
@@ -12,11 +16,8 @@ module csr(
     input wb_stb_i,
     output wb_ack_o,
     input wb_cyc_i);
-parameter NUM_CH = 4;
-parameter VOL_WIDTH = (8*2*NUM_CH);
-parameter ADDR_OFFSET_VOLS = 8'h00;
 
-reg [(VOL_WIDTH):0] vol_ff;
+reg [(VOL_WIDTH-1):0] vol_ff;
 assign vol = vol_ff;
 
 reg wb_ack_ff;
@@ -25,38 +26,29 @@ assign wb_ack_o = wb_ack_ff;
 reg [7:0] wb_dat_ff;
 assign wb_dat_o = wb_dat_ff;
 
+integer i;
+
 always @(posedge clk) begin
     wb_ack_ff <= 0;
+    wb_dat_ff <= 8'h00;
 
     if(rst) begin
-        wb_dat_ff <= 8'h00;
         vol_ff <= {(VOL_WIDTH){1'b0}};
     end else if(wb_stb_i & wb_cyc_i) begin
         wb_ack_ff <= 1;
-    end
-end
 
-always @(posedge clk) begin
-    wb_dat_ff <= 8'h00;
-end
-
-genvar i;
-generate
-    for(i = 0; i < (VOL_WIDTH/8); i = i + 1) begin:s
-        always @(posedge clk) begin
-            if (wb_stb_i & wb_cyc_i) begin
-                if(wb_we_i) begin
-                    // write cmd
-                    if(wb_adr_i == ADDR_OFFSET_VOLS + i)
-                        vol_ff[(i*8+7):(i*8)] <= wb_dat_i;
-                end else begin
-                    // read cmd
-                    if(wb_adr_i == ADDR_OFFSET_VOLS + i)
-                        wb_dat_ff <= vol_ff[(i*8+7):(i*8)];
-                end
+        for(i = 0; i < (VOL_WIDTH/8); i = i + 1) begin
+            if(wb_we_i) begin
+                // write cmd
+                if(wb_adr_i == ADDR_OFFSET_VOLS + i)
+                    vol_ff[i*8 +:8] <= wb_dat_i;
+            end else begin
+                // read cmd
+                if(wb_adr_i == ADDR_OFFSET_VOLS + i)
+                    wb_dat_ff <= vol_ff[i*8 +:8];
             end
         end
     end
-endgenerate
+end
 
 endmodule
--- top.v
+++ top.v
@@ -1,18 +1,50 @@
 `timescale 1ns / 1ps
 
 module top(
-    output tx,
-	output led,
     input clk,
-    input btn
+    input rst,
+
+    output tx,
+    input rx,
+    output led,
+    output led2
     );
 
 assign led = ~tx;
+assign led2 = ~rx;
 
-wire [7:0] data;
-wire txe;
+wire [6:0] wb_adr;
+wire [7:0] wb_dat_ms;
+wire [7:0] wb_dat_sm;
+wire wb_we;
+wire wb_stb;
+wire wb_ack;
+wire wb_cyc;
 
-yes yes(.data(data), .txe(txe), .clk(clk), .btn(btn));
-serialtx serialtx(.tx(tx), .clk(clk), .data(data), .txe(txe));
+serial_wb_if uut(
+    .clk(clk), 
+    .rst(rst),
+    .serial_rx(rx),
+    .serial_tx(tx),
+
+    .wb_adr_o(wb_adr),
+    .wb_dat_i(wb_dat_sm),
+    .wb_dat_o(wb_dat_ms),
+    .wb_we_o(wb_we),
+    .wb_stb_o(wb_stb),
+    .wb_ack_i(wb_ack),
+    .wb_cyc_o(wb_cyc));
+
+csr csr(
+    .clk(clk),
+    .rst(rst),
+
+    .wb_adr_i(wb_adr),
+    .wb_dat_i(wb_dat_ms),
+    .wb_dat_o(wb_dat_sm),
+    .wb_we_i(wb_we),
+    .wb_stb_i(wb_stb),
+    .wb_ack_o(wb_ack),
+    .wb_cyc_i(wb_cyc));
 
 endmodule
